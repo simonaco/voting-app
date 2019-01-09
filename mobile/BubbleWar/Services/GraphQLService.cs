cabac40a8aa7f8962ba64ce14f4aa9d9d650715d
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-01-08 16:01:03 -0800

    Implemented Lazy Loading for IGraphQLAPI

--- mobile/BubbleWar/Services/GraphQLService.cs
+++ mobile/BubbleWar/Services/GraphQLService.cs
@@ -11,17 +11,18 @@ namespace BubbleWar
     public abstract class GraphQLService
     {
         #region Constant Fields
-        readonly static IGraphQLAPI _graphQLApiClient = RestService.For<IGraphQLAPI>(BubbleWarUrl);
+        readonly static Lazy<IGraphQLAPI >_graphQLApiClientHolder = new Lazy<IGraphQLAPI>(()=> RestService.For<IGraphQLAPI>(BubbleWarUrl));
         #endregion
 
         #region Properties
         static string BubbleWarUrl => GraphQLSettings.Uri.ToString();
+        static IGraphQLAPI GraphQLApiClient => _graphQLApiClientHolder.Value;
         #endregion
 
         #region Methods
         public static async Task<List<TeamScore>> GetTeamScoreList()
         {
-            var response = await ExecutePollyFunction(() => _graphQLApiClient.Query(new QueryRequest())).ConfigureAwait(false);
+            var response = await ExecutePollyFunction(() => GraphQLApiClient.Query(new QueryRequest())).ConfigureAwait(false);
 
             if (response.Errors != null)
                 throw new AggregateException(response.Errors.Select(x => new Exception(x.Message)));
@@ -31,7 +32,7 @@ namespace BubbleWar
 
         public static async Task<TeamScore> VoteForTeamAndGetCurrentScore(TeamColor teamType)
         {
-            var response = await ExecutePollyFunction(() => _graphQLApiClient.Mutation(new MutationRequest(teamType))).ConfigureAwait(false);
+            var response = await ExecutePollyFunction(() => GraphQLApiClient.Mutation(new MutationRequest(teamType))).ConfigureAwait(false);
 
             if (response.Errors != null)
                 throw new AggregateException(response.Errors.Select(x => new Exception(x.Message)));
